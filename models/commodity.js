236f1b358a9b2cbf29b2657ac8748ae2f1c2ef52
naufalyunan <naufalyunan45@gmail.com>
2020-02-26 15:19:24 +0700

    finish creating table

---
+++ models/commodity.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  class Commodity extends sequelize.Sequelize.Model {}
+
+  Commodity.init({
+    // Model attributes are defined here
+    name: DataTypes.STRING
+  }, {
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: 'Commodity' // We need to choose the model name
+  });
+  Commodity.associate = function(models) {
+    // associations can be defined here
+    Commodity.hasMany(models.CommodityTrader)
+
+  };
+  return Commodity;
+};
